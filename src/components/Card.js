f14ba096d1881d98d835aad993c2f1d6e70f6057
Javier <jtarulla@gmail.com>
2020-01-08 08:10:20 +0100

    merge

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, email, id }) => {
   return (
     // JSx code, similar to HTML syntax.
     <div className='tc bg-light-blue dib br3 pa0 ma2 grow bw2 shadow-5'>
-      <img alt='friends' src={`https://robohash.org/size=50x50${id}?set=set5`} />
+      <img alt='friends' src={`https://robohash.org/size=50x50${name}?set=set5`} />
       <div> 
         <h2>{name}</h2>
         <p>{email}</p>
